ee8f62615222a56e6bf510e8e907fdbe97673650
sehong3 <a93245124@gmail.com>
2023-09-11 17:53:19 +0900

    add var.py

---
+++ var.py
@@ -0,0 +1,154 @@
+"""my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+set_tmp = set("hello")
+print(set_tmp)
+
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+
+print(my_set - set_item)
+
+print(my_set & set_item)
+
+my_set.add(9)
+
+print(my_set)
+
+my_set.update([5, 9, 7, 4])
+
+my_set.remove(5)
+
+my_set.discard(2)
+
+print(my_set)
+
+my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set)
+
+my_set.difference_update(set_item)
+print(my_set)
+
+my_int = 10
+print(my_int)
+
+print(my_int +10)
+my_str = str(my_int)
+
+print(my_str)
+
+
+print(my_str + " hello")"""
+
+"""my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)
+
+print(my_int + 10)
+
+my_int2 = int("ten")
+print(my_int2)
+
+my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)"""
+
+"""a = 10
+b = 3
+
+#print(a + b)    
+#print(a - b)    
+#print(a * b)    
+#print(a / b)    
+#print(a % b)    
+#print(a // b)   
+#print(a ** b) 
+
+c = a - b
+c = a * b
+c = a / b
+c= a % b
+c = a // b
+c = a ** b""" 
+
+"""a = 0
+print(a)
+
+a += 2
+print(a)
+
+a -= 1
+print(a)
+
+a *= 4
+print(a)
+
+a /= 2
+print(a)
+
+a **= 3
+print(a)"""
+
+"""a = 10
+b = 4
+
+print(a > b)    
+
+print(a < b)    
+
+print(a >= b)   
+
+print(a <= b)   
+
+print(a == b) 
+  
+print(a != b)"""
+
+"""a = 1
+b = 0
+
+print(a and b)  
+print(a or b)   
+print(not a)    
+print(not b)    
+
+x = True
+y = False
+
+print(x and y)  
+print(x or y)   
+print(not x)    
+print(not y)"""    
+
+"""x = 10
+y = 3
+
+print(x & y) 
+  
+print(x | y)   
+
+print(x ^ y)   
+
+print(~x)   
+   
+print(x << 2)"""
+
+my_list = [9, 4, 3, 7, 8, 'hi']
+
+print(4 in my_list)
+
+print(2 in my_list)
+
+print(2 not in my_list)
+
+my_dic = {"key1" : "v1", "k2" : "v2"}
+
+print("k2" in my_dic)
