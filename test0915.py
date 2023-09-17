6d9c2f9e143caaca7fb27f696d9e846b3170ba3f
sehong3 <a93245124@gmail.com>
2023-09-17 19:31:49 +0900

    add test0915.py

---
+++ test0915.py
@@ -0,0 +1,83 @@
+"""a = 5
+b = 5
+
+print(a is b)
+print(a is not b)
+
+
+a = [3 , 5, 1]
+b = a
+
+print(a is b)
+
+a = 2 ** 3 ** 2
+print(a)    
+
+b = 10 / 5 / 2
+print(b)
+
+c = 2 ** (3 ** 2)
+print(c)
+
+d = 10 % 3 % 2
+print(d)
+
+e = 1 + 2 > 3 and 4 - 1 < 2
+print(e)
+
+f = 1 & 2 | 3 ^ 4
+print(f)
+
+g = 5 in [3, 4, 5] or 2 not in [1, 2, 3]
+print(g)
+
+h = 2 * -3 // 2
+print(h)
+
+i = 1== 2 != 3
+print(i)
+
+x = 10
+
+if x > 0:
+    print("x is positive")
+
+elif x < 0:
+    print("x is negative")
+
+else:
+    print("x is zero")
+      
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+    
+my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for row in my_list:
+    for num in row:
+        print(num)
+        
+rang = [5, 8, 3, 1, 6]
+
+for i in rang:
+	print("element : ", i)
+else :
+	print("end process")
+
+for i in range(10):
+    if i == 7:
+        break
+    elif i == 1:
+        continue
+    else:
+        pass
+
+    print(i)"""
+    
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
