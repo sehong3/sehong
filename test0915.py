14258c9989b928d01e98e247ee0b67451e844deb
sehong3 <a93245124@gmail.com>
2023-09-18 17:50:22 +0900

    add test0918.py

--- test0915.py
+++ test0915.py
@@ -74,10 +74,14 @@ for i in range(10):
     else:
         pass
 
-    print(i)"""
+    print(i)
     
 i = 1
 
 while i <= 5:
     print(i)
-    i += 1
+    i += 1"""
+
+for i in range (0, 10): 
+    print(i)
+    
