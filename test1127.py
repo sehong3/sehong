062beb1ebe3b1e73e8e5cd0a1d49114019dec377
sehong3 <143788103+sehong3@users.noreply.github.com>
2023-11-27 17:46:35 +0900

    Update test1127.py

--- test1127.py
+++ test1127.py
@@ -52,4 +52,59 @@ res = df.loc[:, ["지역명", "연도"]][:][:5]
 print(res)
 
 res = df.loc[6:, ["지역명", "연도"]]
+res = df.loc[6:, ["지역명", "연도"]][3:]
+print(res)
+
+res = df.loc[df["지역명"]=="서울",["지역명", "연도", "분양가"]]
+#res = df.loc[df["지역명"]=="서울",["지역명", "연도", "분양가"]][-10]
+print(res)
+
+df0 = df.copy()
+print(df0)
+
+res = df.iloc[2]
+res = df.iloc[2][1]
+print(res)
+
+res = df[df.index > 3560]
+print(res)
+
+res= df[df.월 == 3]
+print(res)
+
+res = df[(df.연도 == 2023) & (df.지역명 == "서울") & (df.월 == 6)]
+print(res)
+
+columns=list(df.columns)
+print(columns)
+
+df1 = df.reindex(columns=list(df.columns)) + ["extra"]
+print(df)
+print("\n--------------------\n")
+print(df1)
+
+df1.loc[:6, "extra"] = '0'
+df1.loc[:4, "extra"] = 'False'
+print(df1)
+
+df2 = df1.copy()
+
+
+res = df2.dropna(how="any", inplace=True)
+res = df2.fillna(how="1234", inplace=True)
+print(df2)
+print("\n--------------------------\n")
+print(res)
+
+res = pd.isna(df1)
+print(res)
+
+res = df["연도"].value_counts()
+#res = df["지역명"].value_counts()
+print(res)
+
+res = df.groupby(["지역명", "연도", "월"]).sum()
+print(res)
+
+res = df.groupby(["지역명", "연도", "월"])["분양가"].agg("sum")
 print(res)
